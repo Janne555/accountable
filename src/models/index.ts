a507125c1c1c452ec86bc15b80f9a4b46bf04e70
Janne Lavila <Janne555@users.noreply.github.com>
2021-05-30 10:46:03 +0000

    implement quarterly

--- src/models/index.ts
+++ src/models/index.ts
@@ -52,7 +52,25 @@ const DateGenerationStrategies = (<T extends Record<string, DateGenerationStrate
     }
   },
   quarterly: (start, end, dayOf) => {
-    return []
+    const fromEnd = dayOf < 0
+    const actualDayOf = Math.abs(dayOf)
+
+    if (actualDayOf === 0 || actualDayOf > 89) {
+      throw Error(`Invalid day of. Must be between 1 and 89`)
+    }
+
+    try {
+      return dateFns.eachQuarterOfInterval({ end, start })
+        .map(date => fromEnd ? dateFns.lastDayOfQuarter(date) : date)
+        .map(date => fromEnd ? dateFns.subDays(date, actualDayOf - 1) : dateFns.addDays(date, actualDayOf - 1))
+        .filter(date => dateFns.isWithinInterval(date, { end, start }))
+    } catch (error) {
+      if (!(error instanceof RangeError)) {
+        throw error
+      } else {
+        return []
+      }
+    }
   },
   yearly: (start, end, dayOf) => {
     return []
