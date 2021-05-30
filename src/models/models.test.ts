813ab8abc81bd6e1a715aeb3a2f1db25f3887eeb
Janne Lavila <Janne555@users.noreply.github.com>
2021-05-30 10:07:17 +0000

    remove exclusive end date requirement

--- src/models/models.test.ts
+++ src/models/models.test.ts
@@ -1,9 +1,9 @@
 import { DateGenerationStrategies } from ".";
 
 describe('DateGenerationStrategies', () => {
-  describe('daily', () => {
+  fdescribe('daily', () => {
     it.each([
-      [2, "2021-05-01T00:00", "2021-05-03T00:00", 1, ["2021-05-01T00:00", "2021-05-02T00:00"]],
+      [2, "2021-05-01T00:00", "2021-05-02T00:00", 1, ["2021-05-01T00:00", "2021-05-02T00:00"]],
       [0, "2021-05-01T00:00", "2021-03-02T00:00", 1, []]
     ])(
       'should generate %d date between %s and %s',
@@ -14,25 +14,19 @@ describe('DateGenerationStrategies', () => {
         expect(result).toMatchObject(expectedResult.map(dateStr => new Date(dateStr)))
       }
     );
-
-    test('end date should be exclusive', () => {
-      const result = DateGenerationStrategies.daily(new Date("2021-05-01T00:00"), new Date("2021-05-02T00:00"), 0)
-      expect(result.length).toBe(1)
-      expect(result).toMatchObject([new Date("2021-05-01T00:00")])
-    });
   });
 
   describe('weekly', () => {
     it.each([
       [2, "2021-05-01T00:00", "2021-05-10T00:00", 0, ["2021-05-02T00:00", "2021-05-09T00:00"]],
       [0, "2021-05-01T00:00", "2021-03-02T00:00", 1, []],
-      [1, "2021-05-03T00:00", "2021-06-10T00:00", 0, ["2021-05-09T00:00"]],
-      [1, "2021-05-03T00:00", "2021-06-10T00:00", 1, ["2021-05-03T00:00"]],
-      [1, "2021-05-03T00:00", "2021-06-10T00:00", 2, ["2021-05-04T00:00"]],
-      [1, "2021-05-03T00:00", "2021-06-10T00:00", 3, ["2021-05-05T00:00"]],
-      [1, "2021-05-03T00:00", "2021-06-10T00:00", 4, ["2021-05-06T00:00"]],
-      [1, "2021-05-03T00:00", "2021-06-10T00:00", 5, ["2021-05-07T00:00"]],
-      [1, "2021-05-03T00:00", "2021-06-10T00:00", 6, ["2021-05-08T00:00"]]
+      [1, "2021-05-03T00:00", "2021-06-09T00:00", 0, ["2021-05-09T00:00"]],
+      [1, "2021-05-03T00:00", "2021-06-09T00:00", 1, ["2021-05-03T00:00"]],
+      [1, "2021-05-03T00:00", "2021-06-09T00:00", 2, ["2021-05-04T00:00"]],
+      [1, "2021-05-03T00:00", "2021-06-09T00:00", 3, ["2021-05-05T00:00"]],
+      [1, "2021-05-03T00:00", "2021-06-09T00:00", 4, ["2021-05-06T00:00"]],
+      [1, "2021-05-03T00:00", "2021-06-09T00:00", 5, ["2021-05-07T00:00"]],
+      [1, "2021-05-03T00:00", "2021-06-09T00:00", 6, ["2021-05-08T00:00"]]
     ])(
       'should generate %d date between %s and %s',
       (dates, start, end, dayOf, expectedResult) => {
@@ -42,12 +36,6 @@ describe('DateGenerationStrategies', () => {
         expect(result).toMatchObject(expectedResult.map(dateStr => new Date(dateStr)))
       }
     );
-
-    test('end date should be exclusive', () => {
-      const result = DateGenerationStrategies.weekly(new Date("2021-05-03T00:00"), new Date("2021-05-16T00:00"), 0)
-      expect(result.length).toBe(1)
-      expect(result).toMatchObject([new Date("2021-05-09T00:00")])
-    });
   });
 
   describe('monthly', () => {
@@ -65,12 +53,6 @@ describe('DateGenerationStrategies', () => {
         expect(result).toMatchObject(expectedResult.map(dateStr => new Date(dateStr)))
       }
     );
-
-    test('end date should be exclusive', () => {
-      const result = DateGenerationStrategies.monthly(new Date("2021-05-01T00:00"), new Date("2021-06-01T00:00"), 1)
-      expect(result.length).toBe(1)
-      expect(result).toMatchObject([new Date("2021-05-01T00:00")])
-    });
 
     it('should only allow up to 28th day', () => {
       const result = DateGenerationStrategies.monthly(new Date("2021-05-01T00:00"), new Date("2021-06-01T00:00"), 29)
@@ -107,12 +89,6 @@ describe('DateGenerationStrategies', () => {
       }
     );
 
-    test('end date should be exclusive', () => {
-      const result = DateGenerationStrategies.quarterly(new Date("2021-01-01T00:00"), new Date("2021-04-01T00:00"), 1)
-      expect(result.length).toBe(1)
-      expect(result).toMatchObject([new Date("2021-05-01T00:00")])
-    });
-
     it('should only allow up to 89th day', () => {
       const result = DateGenerationStrategies.quarterly(new Date("2021-01-01T00:00"), new Date("2021-05-01T00:00"), 90)
       expect(result.length).toBe(1)
@@ -139,12 +115,6 @@ describe('DateGenerationStrategies', () => {
         expect(result).toMatchObject(expectedResult.map(dateStr => new Date(dateStr)))
       }
     );
-
-    test('end date should be exclusive', () => {
-      const result = DateGenerationStrategies.yearly(new Date("2021-01-01T00:00"), new Date("2022-01-01T00:00"), 1)
-      expect(result.length).toBe(1)
-      expect(result).toMatchObject([new Date("2021-01-01T00:00")])
-    });
 
     it('should only allow up to 365th day', () => {
       const result = DateGenerationStrategies.yearly(new Date("2021-01-01T00:00"), new Date("2022-10-01T00:00"), 366)
