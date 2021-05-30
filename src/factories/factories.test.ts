8aea47e3bf31bff3660369d73eacd2f5d91f5a7b
Janne Lavila <Janne555@users.noreply.github.com>
2021-05-30 13:02:27 +0000

    add factory method for events

--- src/factories/factories.test.ts
+++ src/factories/factories.test.ts
@@ -1,8 +1,15 @@
-import { makeSchedule } from ".";
+import { makeEvent, makeSchedule } from ".";
 
 describe('makeSchedule', () => {
   it('should make schedule', () => {
     const schedule = makeSchedule(0, "daily")
+    expect(schedule).toBeDefined()
+  });
+});
+
+describe('makeEvent', () => {
+  it('should make event', () => {
+    const schedule = makeEvent(0, 0, new Date(), [], "historical")
     expect(schedule).toBeDefined()
   });
 });
--- src/factories/index.ts
+++ src/factories/index.ts
@@ -1,10 +1,21 @@
 import { Schedule } from '../models'
-import { ISchedule, ScheduleType } from '../types'
+import { EventType, IEvent, ISchedule, ScheduleType, ICategory } from '../types'
 
 function makeSchedule(dayOf: number, type: ScheduleType): ISchedule {
   return new Schedule(dayOf, type)
 }
 
+function makeEvent(id: number, amount: number, date: Date, categories: ICategory[], type: EventType): IEvent {
+  return {
+    id,
+    amount,
+    date,
+    categories,
+    type
+  }
+}
+
 export {
-  makeSchedule
+  makeSchedule,
+  makeEvent
 }
