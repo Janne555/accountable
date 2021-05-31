9f5137dd642806b613cefe79d84d11be21d694a4
Janne Lavila <Janne555@users.noreply.github.com>
2021-05-31 11:56:00 +0000

    include json logic in opts

--- src/types/workers.d.ts
+++ src/types/workers.d.ts
@@ -2,6 +2,7 @@ namespace Storage {
   interface Options {
     start?: Date
     end?: Date
+    rulesLogic?: import('json-logic-js').RulesLogic
   }
 
   type API = import('../workers/storageWorker').default
