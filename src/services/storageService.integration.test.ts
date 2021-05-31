4cdd7c5b73ed167e9239bb2047de1e6594bbffc5
Janne Lavila <Janne555@users.noreply.github.com>
2021-05-31 10:50:56 +0000

    add tests

--- src/services/storageService.integration.test.ts
+++ src/services/storageService.integration.test.ts
@@ -1,4 +1,4 @@
-import { makeRecurringEvent, makeSchedule } from "../factories";
+import { makeEvent, makeRecurringEvent, makeSchedule } from "../factories";
 import Database from "../misc/database";
 import { RecurringEvent } from "../models";
 import { IEvent, IRecurringEvent } from "../types";
@@ -18,7 +18,7 @@ describe('storageWorker', () => {
   const recurringEvent: IRecurringEvent = makeRecurringEvent([makeSchedule(0, "weekly")], { amount: 5, categories: [], date: new Date("2020-01-01T00:00"), type: "archetype", description: "hello" })
   const recurringEvent2: IRecurringEvent = makeRecurringEvent([makeSchedule(1, "monthly")], { amount: 50, categories: ["asd"], date: new Date("2021-01-01T00:00"), type: "archetype", description: "hello" })
 
-  describe('when getting historical events', () => {
+  describe('historical events', () => {
     beforeEach(async () => {
       db.events.bulkAdd([event, event2, event3, event4])
     })
@@ -44,9 +44,40 @@ describe('storageWorker', () => {
       await expect(storageService.getHistoricalEvents({ start: new Date("2020-01-01T00:00"), end: new Date("2020-01-04T00:00") }))
         .resolves.toMatchObject([event, event3])
     });
+
+    it('should add events', async () => {
+      const newEvent = makeEvent(1, new Date(), [], "historical", "Hello")
+      await storageService.putHistoricalEvents([newEvent])
+      const allEvents = await db.events.toArray()
+      expect(allEvents).toContain(newEvent)
+    });
+
+    it('should modify events', async () => {
+      const oldEvent = await db.events.toCollection().first()
+
+      const modifiedEvent = makeEvent(1, new Date(), [], "historical", "Hello", oldEvent?.id)
+      await storageService.putHistoricalEvents([modifiedEvent])
+
+      const allEvents = await db.events.toArray()
+      expect(allEvents).toContain(modifiedEvent)
+      expect(allEvents).not.toContain(oldEvent)
+    });
+
+    it('should delete events', async () => {
+      const oldEvent = await db.events.toCollection().first()
+
+      if (!oldEvent?.id) {
+        throw Error("invalid test state")
+      }
+
+      await storageService.deleteHistoricalEvents([oldEvent.id])
+
+      const allEvents = await db.events.toArray()
+      expect(allEvents).not.toContain(oldEvent)
+    });
   });
 
-  describe('when getting recurring events', () => {
+  describe('recurring events', () => {
     beforeEach(async () => {
       db.recurringEvents.bulkAdd([recurringEvent, recurringEvent2])
     })
@@ -65,9 +96,40 @@ describe('storageWorker', () => {
       const result = await storageService.getRecurringEvents()
       expect(result[0]).toBeInstanceOf(RecurringEvent)
     });
+
+    it('should add events', async () => {
+      const newEvent = makeRecurringEvent([], makeEvent(1, new Date(), [], "archetype", "Hello"))
+      await storageService.putRecurringEvents([newEvent])
+      const allEvents = await db.recurringEvents.toArray()
+      expect(allEvents).toContain(newEvent)
+    });
+
+    it('should modify events', async () => {
+      const oldEvent = await db.recurringEvents.toCollection().first()
+
+      const modifiedEvent = makeRecurringEvent([], makeEvent(1, new Date(), [], "archetype", "Hello"), oldEvent?.id)
+      await storageService.putRecurringEvents([modifiedEvent])
+
+      const allEvents = await db.events.toArray()
+      expect(allEvents).toContain(modifiedEvent)
+      expect(allEvents).not.toContain(oldEvent)
+    });
+
+    it('should delete events', async () => {
+      const oldEvent = await db.events.toCollection().first()
+
+      if (!oldEvent?.id) {
+        throw Error("invalid test state")
+      }
+
+      await storageService.deleteRecurringEvents([oldEvent.id])
+
+      const allEvents = await db.events.toArray()
+      expect(allEvents).not.toContain(oldEvent)
+    });
   });
 
-  describe('when getting events', () => {
+  describe('events', () => {
     beforeEach(async () => {
       db.events.bulkAdd([event, event2, event3, event4])
       db.recurringEvents.bulkAdd([recurringEvent, recurringEvent2])
