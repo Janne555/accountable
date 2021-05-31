21de2f7d52ec847009264208af720d7cc30f568b
Janne Lavila <Janne555@users.noreply.github.com>
2021-05-31 16:28:05 +0000

    proxy callbacks

--- src/workers/storageWorkerClient.ts
+++ src/workers/storageWorkerClient.ts
@@ -3,6 +3,16 @@ import { Storage } from '../types';
 
 const storageWorker = new Worker(new URL('./storageWorker', import.meta.url))
 
-const storageWorkerClient: Storage.API = Comlink.wrap(storageWorker) as unknown as Storage.API
+const comlinkAPI = Comlink.wrap<Storage.API>(storageWorker)
+
+const storageWorkerClient: Storage.API = {
+  getEvents: (opts, cb) => comlinkAPI.getEvents(opts, Comlink.proxy(cb)),
+  getHistoricalEvents: (opts, cb) => comlinkAPI.getHistoricalEvents(opts, Comlink.proxy(cb)),
+  putHistoricalEvents: (events, cb) => comlinkAPI.putHistoricalEvents(events, Comlink.proxy(cb)),
+  deleteHistoricalEvents: (ids, cb) => comlinkAPI.deleteHistoricalEvents(ids, Comlink.proxy(cb)),
+  getRecurringEvents: (opts, cb) => comlinkAPI.getRecurringEvents(opts, Comlink.proxy(cb)),
+  putRecurringEvents: (events, cb) => comlinkAPI.putRecurringEvents(events, Comlink.proxy(cb)),
+  deleteRecurringEvents: (ids, cb) => comlinkAPI.deleteRecurringEvents(ids, Comlink.proxy(cb))
+}
 
 export default storageWorkerClient
