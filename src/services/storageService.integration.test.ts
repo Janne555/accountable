766c4585ddbf138451644ca5ea89e4d784be38f0
Janne Lavila <Janne555@users.noreply.github.com>
2021-05-31 08:22:50 +0000

    add new test

--- src/services/storageService.integration.test.ts
+++ src/services/storageService.integration.test.ts
@@ -1,5 +1,6 @@
+import { makeRecurringEvent, makeSchedule } from "../factories";
 import Database from "../misc/database";
-import { IEvent } from "../types";
+import { IEvent, IRecurringEvent } from "../types";
 import StorageWorker from "../workers/storageWorker";
 import StorageService from "./storageService";
 
@@ -12,6 +13,9 @@ describe('storageWorker', () => {
   const event2: IEvent = { amount: 0, categories: [], date: new Date("2020-02-01T00:00"), type: "historical" }
   const event3: IEvent = { amount: 0, categories: [], date: new Date("2020-01-03T00:00"), type: "historical" }
   const event4: IEvent = { amount: 0, categories: [], date: new Date("2004-01-01T00:00"), type: "historical" }
+
+  const recurringEvent: IRecurringEvent = makeRecurringEvent([makeSchedule(0, "weekly")], { amount: 0, categories: [], date: new Date("2020-01-01T00:00"), type: "archetype" })
+  const recurringEvent2: IRecurringEvent = makeRecurringEvent([makeSchedule(1, "monthly")], { amount: 50, categories: ["asd"], date: new Date("2021-01-01T00:00"), type: "archetype" })
 
   describe('when getting historical events', () => {
     beforeEach(async () => {
@@ -38,6 +42,21 @@ describe('storageWorker', () => {
     it('should filter by start and end date', async () => {
       await expect(storageService.getHistoricalEvents({ start: new Date("2020-01-01T00:00"), end: new Date("2020-01-04T00:00") }))
         .resolves.toMatchObject([event, event3])
+    });
+  });
+
+  describe('when getting recurring events', () => {
+    beforeEach(async () => {
+      db.recurringEvents.bulkAdd([recurringEvent, recurringEvent2])
+    })
+
+    afterEach(async () => {
+      await db.events.clear()
+      await db.recurringEvents.clear()
+    })
+
+    it('should get them', async () => {
+      await expect(storageService.getRecurringEvents()).resolves.toMatchObject([recurringEvent, recurringEvent2])
     });
   });
 });
