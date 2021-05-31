1466cdffb592ae17bc34b9d7aa9d6345d52fbd13
Janne Lavila <Janne555@users.noreply.github.com>
2021-05-31 12:16:01 +0000

    add storage service to types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,5 @@
 export * from './models'
 export * from './workers'
 export * from './utils'
+export * from './services'
 export type Database = import('../misc/database').default
---
+++ src/types/services.d.ts
@@ -0,0 +1,5 @@
+type StorageService = import('../services').storageService
+
+export {
+  StorageService
+}
