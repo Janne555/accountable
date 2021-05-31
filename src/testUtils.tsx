c947ee127691fc3f7d29ff171def2bcf8906a3d2
Janne Lavila <Janne555@users.noreply.github.com>
2021-05-31 15:36:51 +0000

    add query cache

--- src/testUtils.tsx
+++ src/testUtils.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { DependencyProvider } from "./hooks/useDependencies";
 import { Storage } from './types';
+import { QueryClientProvider, QueryClient } from 'react-query'
 
-function createContextWrapper(storageWorkerClient: Storage.API) {
+function createContextWrapper(storageWorkerClient: Storage.API, queryClient: QueryClient = new QueryClient()) {
   return function Wrapper({ children }: { children: React.ReactNode }) {
     return (
-      <DependencyProvider storageWorkerClient={storageWorkerClient}>
-        {children}
-      </DependencyProvider>)
+      <QueryClientProvider client={queryClient}>
+        <DependencyProvider storageWorkerClient={storageWorkerClient}>
+          {children}
+        </DependencyProvider>
+      </QueryClientProvider>
+    )
   }
 }
 
