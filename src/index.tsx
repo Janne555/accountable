f202690cd63d66323ef1e919a97b51a851e50341
Janne Lavila <Janne555@users.noreply.github.com>
2021-06-01 10:53:50 +0300

    add json editor css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'jsoneditor/dist/jsoneditor.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
